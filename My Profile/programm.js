fc9ea67de5c9700e16376c1f29d8311c60c1a1c4
Md Apon <81330099+apon2021@users.noreply.github.com>
2021-04-12 03:52:55 +0600

    new project

---
+++ My Profile/programm.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+
+
+
+var typed = new Typed('.type', {
+  strings: [
+      
+    
+   'I am a new WebDesigner',
+   'I learn web development',
+   'I have 90% HTML skill',
+   'I have 90% CSS skill',
+   'I have 50% Javascript skill',
+   'I have 40% Boostrap skill',
+   'I have 30% Jquery skill',
+
+
+
+],
+    
+  typeSpeed: 200,
+  loop:true,
+
+
+});
+
+})
